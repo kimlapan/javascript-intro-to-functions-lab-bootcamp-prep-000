73f565fbdd3064749899da99c4a2961e557c21e7
Kim L <kimlapan@gmail.com>
2018-06-12 00:46:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ function shout(string) {
   return 
   string.toUpperCase()
 }
+
+function testNum(a) {
+  if (a > 0) {
+    return "positive";
+  } else {
+    return "NOT positive";
+  }
+}
+
+console.log(testNum(-5));
